6e4eac9cc0d3d1509bf4a9e095559d8702a81976
Lucas Coon <lucascoon@yahoo.com>
2020-03-07 13:45:09 -0600

    math changes

--- LIDAR/new-lidar.ino
+++ LIDAR/new-lidar.ino
@@ -74,7 +74,7 @@ void loop() {
     while(Serial.available() && number < INPUT_SIZE)
     {
       
-      x = Serial.read();
+     x = Serial.read();
      Serial.println(number);
      storage[number] = x;
      number++;
@@ -84,22 +84,28 @@ void loop() {
    digitalWrite(Relay, LOW);
      for(int j = 0; j < (INPUT_SIZE-8) && processing == 0; j++)
      {
-       Serial.println(storage[j]);
-       if((storage[j-6] == 0xA5) && (storage[j-5] == 0x5A) && (storage[j] == (0x81)))
+       //Serial.println(storage[j]);
+       if((storage[j] == 0xA5) && (storage[j+1] == 0x5A) && (storage[j+6] == (0x81)))
        {
          Serial.println("FOUND");
+         counting=j+7;
          processing = 1; //Starts doing "Post-processing of current lidar sample set
        }
        else
        {
-        delay(1000);
+        delay(10);
         
-        number = 0;
+        //number = 0;
       //Trigger Relay to restart lidar
           //Serial.println(storage[0], HEX);
           //Serial.println(storage[1], HEX);
        }
+
      }
+   if (processing == 0)
+   {
+      number = 0;
+   }
   while(processing)
   {
     digitalWrite(Relay, LOW);
@@ -108,39 +114,60 @@ void loop() {
     int Start_Pos = 0;
     int End_ang = 0;
     int End_Pos = 0;
+    double store_angle = 0;
+    int sort_size = number-counting;
     int count = 0; //used for the actual sorting
-    int sorting[counting][2];//used to store the distances assigned to there angle
-    int angle[counting];//used to store intermediate angles
-    for(int j=8; j<counting; j++) //J is 8 because in the first 7 Bytes only the first 2 are important
+    double sorting[sort_size][2];//used to store the distances assigned to there angle
+    for(int j=counting; j<number; j++) //J is 8 because in the first 7 Bytes only the first 2 are important
     {
-      if(storage[j] == 0xAA && storage[j+1] == 0x55)
+      //Serial.println(storage[j]);
+      if(storage[j] == 0xAA && storage[j+1] == 0x55 && storage[j+3] > 20)
       {
+ 
         j+=3; //walks to the LSN
         sample_rate=storage[j];
+        double angle[sample_rate];//used to store intermediate angles
+        delay(10);
+        Serial.println(sample_rate);
         j++; //walks to the FSA
         Start_ang = math(storage[j], storage[j+1]);
         Start_ang = angle_calc(Start_ang);
-        j+=2;//walks to the LSA
+        Serial.println(Start_ang);
+        j= j+2;//walks to the LSA
         End_ang = math(storage[j], storage[j+1]);
         End_ang = angle_calc(End_ang);
+        Serial.println(End_ang);
         j+=4;//walks to the first sample data
         Start_Pos = j;
-        End_Pos = j+sample_rate;
-        for(int i=2; i<sample_rate; i++) // Does intermediate angle calculations
+        End_Pos = j + sample_rate;
+        for(int i=2; i < sample_rate; i++) // Does intermediate angle calculations
         {
-          angle[i] = (((End_ang-Start_ang)/(sample_rate-1))*(i-1)+Start_ang);
+          Serial.println(Start_ang);
+          Serial.println(End_ang);
+          Serial.println(sample_rate);
+          store_angle = ((End_ang - Start_ang)/(sample_rate - 1));
+          Serial.println(store_angle);
+          //Serial.println(((store_angle*(i-1))+Start_ang));
+          angle[i] = ((store_angle * (i - 1)) + Start_ang);
+          delay(10);
         }
-      }
+      
       for(count; count < ((End_Pos - Start_Pos)+count); (count+=2))
       {
-        sorting[((count)/2)][0] = math(storage[count+Start_Pos], storage[count+Start_Pos+1]);
+        sorting[((count)/2)][0] = (math(storage[count+Start_Pos], storage[count+Start_Pos+1]))/64;
         sorting[((count)/2)][1] = angle[((count)/2)];
       }
       j=j+count;
+      //for(int i=0; i < sample_rate; i++)
+      //{
+        //Serial.println(angle[i]);
+      //}
     }
-    //processing = 0;
-    
-  }
+    }
+    processing = 0;
+
+
+}
 }
 
 
@@ -168,6 +195,7 @@ int angle_calc(int x)
   int theta = 0;
   theta = x >> 1;
   theta = theta/64;
+  Serial.println(theta);
   return theta;
 }
 
@@ -182,10 +210,11 @@ int angle_calc(int x)
 int math(int a, int b)
 {
   int storage = b;
-  Serial.println(b, BIN);
+  //Serial.println(a);
+  //Serial.println(b);
   storage= b<<8;
-  Serial.println(storage, BIN);
+  //Serial.println(storage);
   storage = storage|a;
-  //Serial.println(value, BIN);
+  //Serial.println(storage);
   return storage;
 }
